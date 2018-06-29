15785a469412da6d8de6356b916143bc82f90759
Alison Shin <alisonyshin@gmail.com>
2018-06-29 07:36:18 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board,index)
   if (board[index] = "X") || (board[index] = "O") == TRUE
     return TRUE
-  elsif board[index] = " "
+  elsif (board[index] = " ") ||
     return FALSE
   end
 end
